8badf998c632189c5445d6f6bf48324d974f94e6
Cleidiano Oliveira de Souza <oliveiracleidiano@gmail.com>
2018-08-11 16:44:56 -0300

    Add unit test

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,17 @@ pub struct Config {
 }
 
 
+pub fn run(config: Config) -> Result<(), Box<Error>> {
+    let mut file = File::open(config.filename)?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
+    Ok(())
+}
+
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
@@ -21,11 +32,29 @@ impl Config {
     }
 }
 
-pub fn run(config: Config) -> Result<(), Box<Error>> {
-    let mut file = File::open(config.filename)?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    println!("With text:\n{}", contents);
-    Ok(())
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "Rust: \nsafe, fast, productive\n
+        Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive"],
+                   search(query, contents)
+        );
+    }
+}
